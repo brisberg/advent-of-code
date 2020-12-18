4ad58e48f01d2d39059723b282b6a4e2516ab266
brisberg <brandon.risberg@gmail.com>
2020-12-18 07:45:47 -0800

    7: Implemented puzzle 7.A

--- 2020/7/A/puzzleA.ts
+++ 2020/7/A/puzzleA.ts
@@ -1,3 +1,4 @@
+import {parse} from './rule';
 
 /**
  * Specialized PuzzleFunction for Puzzle 7.A.
@@ -6,7 +7,41 @@
  * 'shiny gold' bags. Returns the count of these containing bags.
  */
 export function puzzleA(inputs: string[]): number {
-  // const answers = readGroups(inputs).map(unionAnswers);
-  // return answers.reduce((sum, answer) => sum + answer.length, 0);
-  return 0;
+  const rules = inputs.map(parse);
+
+  // Inverse mapping. Mapping a bag label to the bag labels which can directly
+  // contain it.
+  const canBeContainedIn: {[label: string]: string[]} = {};
+
+  // Build inverse mapping
+  rules.forEach((rule) => {
+    Object.keys(rule.contains).forEach((sublabel) => {
+      if (!canBeContainedIn[sublabel]) {
+        canBeContainedIn[sublabel] = [];
+      }
+      canBeContainedIn[sublabel].push(rule.label);
+    });
+  });
+
+  // Recursively search container map to exhaust all top level bags
+  const labelsToCheck = new Set(['shiny gold']);
+  const resultSet = new Set<string>();
+  while (labelsToCheck.size > 0) {
+    const label: string = labelsToCheck.values().next().value;
+    labelsToCheck.delete(label);
+
+    const containers = canBeContainedIn[label];
+
+    if (containers) {
+      containers.forEach((container) => {
+        labelsToCheck.add(container);
+        resultSet.add(container);
+      });
+    }
+  }
+
+  const results = Array.from(resultSet);
+  console.log(`Top-level bags which can contain 'shiny gold' bags are:\n${
+      results.join(', ')}`);
+  return results.length;
 }
