7717cff38dbb2188357a801afcefb155e560e209
brisberg <brandon.risberg@gmail.com>
2020-12-18 07:23:10 -0800

    7: Added shell for puzzleA and test

---
+++ 2020/7/A/puzzleA.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Specialized PuzzleFunction for Puzzle 7.A.
+ *
+ * Read all rule inputs, builds a bag dependency tree, reads the containers of
+ * 'shiny gold' bags. Returns the count of these containing bags.
+ */
+export function puzzleA(inputs: string[]): number {
+  // const answers = readGroups(inputs).map(unionAnswers);
+  // return answers.reduce((sum, answer) => sum + answer.length, 0);
+  return 0;
+}
