39c368adf9abd50b98fa1995856b10c6be1518d9
brisberg <brandon.risberg@gmail.com>
2020-12-20 19:45:18 -0800

    14: Added shell, prompt, test case for 14/A

---
+++ 2020/14/A/puzzleA.spec.ts
@@ -0,0 +1,19 @@
+import {puzzleA} from './puzzleA';
+
+describe.skip('PuzzleA', () => {
+  it('should process all instructions and return the sum of values in memory',
+     () => {
+       const inputs = [
+         'mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+         'mem[8] = 11',
+         'mem[7] = 101',
+         'mem[8] = 0',
+       ];
+
+       // In the above example, only two values in memory are not zero - 101 (at
+       // address 7) and 64 (at address 8) - producing a sum of 165.
+
+       const result = puzzleA(inputs);
+       expect(result).toEqual(165)
+     });
+});
---
+++ 2020/14/A/puzzleA.ts
@@ -0,0 +1,9 @@
+/**
+ * Specialized PuzzleFunction for Puzzle 14.A.
+ *
+ * Initialize a memory processor. Execute all instructions. Return the sum of
+ * all the values in memory.
+ */
+export function puzzleA(inputs: string[]): number {
+  return 0
+}
